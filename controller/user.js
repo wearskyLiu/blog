f40d0c2b6fab2cbe6c4db3c4e9b3e55f644e24c3
wearsky <wearsky@koiyan.com>
2023-11-03 17:24:59 +0800

    commit

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,6 @@
 const Models = require('../db/Models.js');
 const crypto = require('../util/crypto.js');
-const jwt = require('../util/jwt.js');
+//const jwt = require('../util/jwt.js');
 
 module.exports.register = async (req, res) => {
     try {
@@ -10,11 +10,11 @@ module.exports.register = async (req, res) => {
         await newUser.save();
         req.session.userInfor = { id: newUser._id.toString() };
 
-        res.status(201).json(
+        res.status(200).json(
             { id: newUser._id.toString() }
         )
     } catch (err) {
-        res.status(500).json({ value: "false" });
+        res.status(500).json({ value: 'err' });
     }
 }
 
@@ -24,14 +24,16 @@ module.exports.login = async (req, res) => {
         if (user) {
             req.body.password = crypto(req.body.password, 'md5');
             if (req.body.password === user.password) {
-                let jwtCode = jwt.sign({ id: user._id.toString() })
-                return res.status(200).json({ id: user._id.toString(), authorization: jwtCode })
+                //let jwtCode = jwt.sign({ id: user._id.toString() })
+                //return res.status(200).json({ id: user._id.toString(), authorization: jwtCode })
+                req.session.userInfor = { id: user._id.toString() };
+                return res.status(200).json({ id: user._id.toString() })
             }
-            return res.status(404).json({ value: "email or pswd error" });
+            return res.status(401).json({ value: "email or pswd error" });
         }
-        res.status(404).json({ value: "email or pswd error" });
+        res.status(404).json({ value: "user not found" });
     } catch (err) {
-        console.log(err)
-        res.status(500).json({ value: err });
+        //console.log(err)
+        res.status(500).json({ value: 'err' });
     }
 }
--- router/article.js
+++ router/article.js
@@ -35,31 +35,32 @@ router.get('/home/page/:id', async (req, res) => {
         let page = (parseInt(req.params.id) - 1) * 10;
         let articles = await Models.articleModel.find().sort('-createdDate').limit(10).skip(page).exec();
         let count = await Models.articleModel.find().count();
+        //console.log(req.session.userInfor)
 
         if (articles.length > 0) {
             if (parseInt(req.params.id) > 6) {
-                return res.render('home2', { article: articles, page: Array.from(Array(parseInt(req.params.id)).keys(), n => n + 1).slice(-6), current: req.params.id })
+                return res.render('home', { article: articles, page: Array.from(Array(parseInt(req.params.id)).keys(), n => n + 1).slice(-6), current: req.params.id, mode: 1, auth: req.session.userInfor === undefined ? 0 : 1 })
             }
-            console.log(Array.from(Array(parseInt(req.params.id)).keys(), n => n + 1))
-            return res.render('home2', { article: articles, page: Array.from(Array(Math.ceil(count / 10)).keys(), n => n + 1), current: req.params.id })
+            //console.log(Array.from(Array(parseInt(req.params.id)).keys(), n => n + 1))
+            return res.render('home', { article: articles, page: Array.from(Array(Math.ceil(count / 10)).keys(), n => n + 1), current: req.params.id, mode: 1, auth: req.session.userInfor === undefined ? 0 : 1 })
         }
     } catch (error) {
-        res.status(500).send('err');
+        res.status(500).send(error);
     }
 })
 
 router.get('/home', async (req, res) => {
     let articles = await Models.articleModel.find().sort('-createdDate').limit(10).skip(0).exec();
     let count = await Models.articleModel.find().count();
-    console.log(Math.ceil(count / 10));
-    console.log(Array.from(Array(Math.ceil(count / 10)).keys(), n => n + 1))
+    //console.log(Math.ceil(count / 10));
+    //console.log(Array.from(Array(Math.ceil(count / 10)).keys(), n => n + 1))
 
     if (Math.ceil(count / 10) > 6) {
 
-        return res.render('home', { article: articles, page: [1, 2, 3, 4, 5, 6] })
+        return res.render('home', { article: articles, page: [1, 2, 3, 4, 5, 6], mode: 0, auth: req.session.userInfor === undefined ? 0 : 1 })
     }
 
-    return res.render('home', { article: articles, page: Array.from(Array(Math.ceil(count / 10)).keys(), n => n + 1) })
+    return res.render('home', { article: articles, page: Array.from(Array(Math.ceil(count / 10)).keys(), n => n + 1), mode: 0, auth: req.session.userInfor === undefined ? 0 : 1 })
 })
 
 module.exports = router;
